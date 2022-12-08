cae8e4bde02f23c1da6293d34ce8e0e052ba31b0
lumipe <pelubabe6@gmail.com>
2022-12-08 15:35:57 -0800

    changes recorrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,16 @@
  */
 int main(void)
 {
-	printf("size of int: %i byte(s)\n", sizeof(int));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 	printf("size of char: %i byte(s)\n", sizeof(char));
-	printf("size of float: %i byte(s)\n", sizeof(float));
+	printf("size of int: %i byte(s)\n", sizeof(int));
 	printf("size of long int: %i byte(s)\n", sizeof(long int));
 	printf("size of long long int: %i byte(s)\n", sizeof(long long int));
+	printf("size of float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
