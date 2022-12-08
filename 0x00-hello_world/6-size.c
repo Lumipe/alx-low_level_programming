f41b5a5b09ae31de813ef6ce33219c4e034ab506
lumipe <pelubabe6@gmail.com>
2022-12-08 09:10:14 -0800

    created 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("size of int: %i byte(s)\n", sizeof(int));
+	printf("size of char: %i byte(s)\n", sizeof(char));
+	printf("size of float: %i byte(s)\n", sizeof(float));
+	printf("size of double: %i byte(s)\n", sizeof(double));
+	return (0);
+}
