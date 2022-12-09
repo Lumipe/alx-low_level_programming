cbc2eed0087ae999209c155ede37dfd9fb357e62
lumipe <pelubabe6@gmail.com>
2022-12-08 16:16:25 -0800

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 1(Error)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+	write(2, err, 59);
 	return (1);
 }
