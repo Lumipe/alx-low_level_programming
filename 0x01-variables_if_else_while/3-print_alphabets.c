4ec0a9f4567e1784d6c76f19a7866481585d2379
lumipe <pelubabe6@gmail.com>
2022-12-11 15:57:26 -0800

    added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets in lower and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lu;
+
+	for (lu = 'a'; lu = 'z'; lu++)
+		putchar(lu);
+	for (lu = 'A'; lu = 'Z'; lu++)
+		putchar(lu);
+	putchar('\n');
+	return (0);
+}
