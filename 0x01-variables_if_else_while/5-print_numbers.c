cb6e0b8ddac71141296967218aef9520accf344f
lumipe <pelubabe6@gmail.com>
2022-12-11 16:56:47 -0800

    created numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing all single digit numbers of base 10
+ * Return: Always 0
+ */
+int main(0)
+{
+	dig;
+	for (dig = '0'; dig <= '9'; dig++)
+		printf("%d", dig);
+
+	printf("\n");
+
+	return (0);
+}
