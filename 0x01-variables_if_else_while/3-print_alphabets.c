53ef8961ef4d471073aa7ff372c4bb22fe83dc3f
lumipe <pelubabe6@gmail.com>
2022-12-11 16:12:04 -0800

    added a change

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,13 @@ int main(void)
 {
 	char lu;
 
-	for (lu = 'a'; lu = 'z'; lu++)
+	for (lu = 'a'; lu <= 'z'; lu++)
 		putchar(lu);
-	for (lu = 'A'; lu = 'Z'; lu++)
+
+	for (lu = 'A'; lu <= 'Z'; lu++)
 		putchar(lu);
+
 	putchar('\n');
+
 	return (0);
 }
