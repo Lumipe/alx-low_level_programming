85353bdb347ca3278e3eaff28dde05e80d06e95a
lumipe <pelubabe6@gmail.com>
2022-12-12 12:37:31 -0800

    created 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - printing all possible different combination of digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num1, num2;
+	/*ASCII code for 0*/
+	num1 = 48;
+
+	while (num1 < 58)
+	{
+		num2 = 48;
+		while (num2 < 58)
+		{
+			putchar(num1);
+			putchar(num2);
+			num2++;
+			putchar(',');
+			putchar(' ');
+		}
+
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
