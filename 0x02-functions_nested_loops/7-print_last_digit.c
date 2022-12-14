f2a05f33b6bc710958d738b52adb72093d8fc68c
lumipe <pelubabe6@gmail.com>
2022-12-13 16:19:51 -0800

    added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: number
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int a = int n
+
+	int a = -(n % 10);
+	
+	putchar(a + '0');
+	return (0);
+}
