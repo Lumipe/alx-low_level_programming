0e3e8442b3f334890743d5e6f9bc54542fedf990
lumipe <pelubabe6@gmail.com>
2022-12-14 17:41:45 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,22 @@
 
 /**
  * print_last_digit - print the last digit of a number
- * @n: number
+ * @n: number to be printed
  * Return: value of last digit
  */
 int print_last_digit(int n)
 {
-	int a = int n
+	int l;
 
-	int a = -(n % 10);
-	
-	putchar(a + '0');
-	return (0);
+	if (n < 0)
+		n = -n;
+
+	l = n % 10;
+
+	if (l < 0)
+		l = -l;
+
+	_putchar(l + '0');
+
+	return (l);
 }
